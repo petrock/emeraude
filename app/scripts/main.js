ab0675ebafc8967e3e1848cbe9d783365adcf1de
Mark Koyanagi <blindzombie@gmail.com>
2014-09-24 07:59:56 -0400

    Added d3 function to append verse one text
    Uses lodash _.filter to grab only verse 1 lines
    
    TODO:
    Set default verse to 1, add auto increment to change verse

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log(poem);
+function showText() {
+  var verseOne = _.filter(poem.text, function(text) {return text.verse === 1});
+  console.log(verseOne);
+  d3.select('.text').selectAll('div')
+      .data(verseOne)
+    .enter().append('div').attr('class', 'line')
+      .text(function(d) {return d.text;});
+}
+
+showText();
